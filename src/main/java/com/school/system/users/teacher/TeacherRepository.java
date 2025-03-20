81fbca846e0294375861a9531ed02974584702e2
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2025-03-20 22:21:06 +0200

    Added user, student and teacher repositories

---
+++ src/main/java/com/school/system/users/teacher/TeacherRepository.java
@@ -0,0 +1,10 @@
+package com.school.system.users.teacher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
+}
