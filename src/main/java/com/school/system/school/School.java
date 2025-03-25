a879a589ddcf6838d5db5e0749973978a761ec4a
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2025-03-25 20:51:32 +0200

    added grade and school entity

---
+++ src/main/java/com/school/system/school/School.java
@@ -0,0 +1,38 @@
+package com.school.system.school;
+
+import com.school.system.grade.Grade;
+import com.school.system.users.student.Student;
+import com.school.system.users.teacher.Teacher;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "schools")
+public class School {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL)
+    private List<Grade> classes;
+
+    @ManyToMany(mappedBy = "schools", cascade = CascadeType.ALL)
+    private List<Teacher> teachers;
+
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL)
+    private List<Student> students;
+}
