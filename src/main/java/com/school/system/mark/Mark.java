0cd246ce490b22f3ceee2d717b65bacc3770b86d
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2025-03-29 00:34:55 +0200

    changed createdAt in Mark

--- src/main/java/com/school/system/mark/Mark.java
+++ src/main/java/com/school/system/mark/Mark.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -37,5 +37,5 @@ public class Mark {
     private Integer value;
 
     @Column(nullable = false)
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/school/system/mark/MarkResponseDTO.java
+++ src/main/java/com/school/system/mark/MarkResponseDTO.java
@@ -4,12 +4,12 @@ import com.school.system.subject.SubjectResponseDTO;
 import com.school.system.users.student.StudentResponseDTO;
 import com.school.system.users.teacher.TeacherResponseDTO;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public record MarkResponseDTO(
         SubjectResponseDTO subject,
         StudentResponseDTO student,
         TeacherResponseDTO teacher,
         int value,
-        LocalDate createdAt
+        LocalDateTime createdAt
 ) {}
