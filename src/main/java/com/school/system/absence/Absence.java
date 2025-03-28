e568512c2df57f78edf0022744cd468cc6742ec8
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2025-03-28 23:04:36 +0200

    created absence entity

---
+++ src/main/java/com/school/system/absence/Absence.java
@@ -0,0 +1,38 @@
+package com.school.system.absence;
+
+import com.school.system.subject.Subject;
+import com.school.system.users.student.Student;
+import com.school.system.users.teacher.Teacher;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "absences")
+public class Absence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    private Teacher author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+
+    @Column(nullable = false)
+    private LocalDate createdAt;
+}
