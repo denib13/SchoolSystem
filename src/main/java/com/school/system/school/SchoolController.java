7ec62a78e05061556b4d555d8d15efdef969beda
Denitsa Belicheva <65655906+denib13@users.noreply.github.com>
2025-04-01 20:27:39 +0300

    added pagination for school

--- src/main/java/com/school/system/school/SchoolController.java
+++ src/main/java/com/school/system/school/SchoolController.java
@@ -2,6 +2,7 @@ package com.school.system.school;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +26,11 @@ public class SchoolController {
     }
 
     @GetMapping
-    public ResponseEntity<List<SchoolResponseDTO>> getSchools() {
-        return new ResponseEntity<>(schoolService.getSchools(), HttpStatus.OK);
+    public ResponseEntity<Page<SchoolResponseDTO>> getSchools(
+            @RequestParam(name = "pageNo", defaultValue = "0", required = false) int pageNo,
+            @RequestParam(name = "pageSize", defaultValue = "5", required = false) int pageSize
+    ) {
+        return new ResponseEntity<>(schoolService.getSchools(pageNo, pageSize), HttpStatus.OK);
     }
 
     @PutMapping("{id}")
